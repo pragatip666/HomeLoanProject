9206c170899a5d31cf57236af426e32fdf3fbcf9
Rohit Goyal <rg.rohitgoyal01@gmail.com>
2020-08-27 22:13:23 +0530

    Minor changes

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -2,12 +2,6 @@ import { Router } from '@angular/router';
 import { RegisterService } from './../register.service';
 import { Component, OnInit } from '@angular/core';
 
-//import { RegistrationService} from '../registration.service';
-//import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-// import custom validator to validate that password and confirm password fields match
-//import { MustMatch } from './_helpers/must-match.validator';
-
 @Component({
   selector: 'registration',
   templateUrl: './registration.component.html',
